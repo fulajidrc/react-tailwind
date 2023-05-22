78244bc5b7bbfb0f272e2319768f567c8eed57b0
Fulaji Thakor <fulaji.thakor@drcsystems.com>
2023-05-22 17:17:47 +0530

    add tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   jit: true,
   darkMode: 'class',
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
     extend: {},
   },
